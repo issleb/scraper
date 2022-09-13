95c927478d5a73126f1c819fa1dac0f365e914a9
Brian Issleb <issleb@gmail.com>
2022-09-13 13:15:40 -0500

    Get title.

--- parser.py
+++ parser.py
@@ -2,13 +2,23 @@ from dataclasses import dataclass
 import re
 
 def get_movie(tag):
+    title = _get_title(tag)
     year = _get_year(tag)
 
     movie = Movie()
+    movie.title = title
     movie.year = year
 
     return movie
 
+def _get_title(text):
+    regex = r">([^<]*)</A>"
+    result = re.search(regex, text)
+
+    if not result:
+        return
+
+    return result.group(1)
 
 def _get_year(text):
     regex = r"/A> \((....)"
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,4 +5,7 @@ def test_first():
     DATA = '<P><A HREF="/reviews/reviewsa-d/abyss.htm">The Abyss</A> (1989) **½</P>'
     result =  parser.get_movie(DATA)
 
+    print(result)
+
+    assert result.title == "The Abyss"
     assert result.year == "1989"
