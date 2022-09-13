1945d00cccc4d6a633e8bb3b64818d78cfb772c9
Brian Issleb <issleb@gmail.com>
2022-09-13 10:36:30 -0500

    Output to file.

--- scraper.py
+++ scraper.py
@@ -1,6 +1,12 @@
 import requests
+from contextlib import redirect_stdout
 
 URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
 
-print(page.text)
+# print(page.text)
+
+
+with open('output/out.txt', 'w') as f:
+    with redirect_stdout(f):
+        print(page.text)
